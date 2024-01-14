74a40594c87931c93b43ba349cf3890f991bb89e
SamuelMozarth <contatosamuelm@gmail.com>
2024-01-14 19:36:55 -0300

    fix bug

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:samuelmozarth_portifolio/main.dart';
+import 'package:samuelmozarth_portifolio/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomeScreen());
 
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
