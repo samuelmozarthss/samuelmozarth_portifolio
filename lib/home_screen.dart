ad09f17a7ad7e82d35300ab2ea25cfeacbfd3dd3
SamuelMozarth <contatosamuelm@gmail.com>
2024-01-14 23:48:44 -0300

    create header and socialmedia icons

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,34 +10,93 @@ class HomeScreen extends StatelessWidget {
       title: 'Samuel Mozarth',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.black87,
-        body: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-                child: const Center(
-                  child: Text(
-                    'Metade 1',
-                    style: TextStyle(color: Colors.white),
-                  ),
+        body: Padding(
+          padding:
+              const EdgeInsets.only(left: 48, top: 80, right: 48, bottom: 80),
+          child: Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Samuel Mozarth',
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 64,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Mobile Software Engineer',
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 36,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '"It is during the battle that we evolve."',
+                      style: GoogleFonts.inter(
+                        textStyle: const TextStyle(
+                          fontSize: 24,
+                          color: Color.fromRGBO(0, 0, 0, 0.6),
+                        ),
+                      ),
+                    ),
+                    Flexible(
+                      child: Container(),
+                    ),
+                    Row(
+                      children: [
+                        Image.asset(
+                          'assets/images/Github.png',
+                          width: 50,
+                          height: 50,
+                        ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Image.asset(
+                          'assets/images/Linkedin.png',
+                          width: 50,
+                          height: 50,
+                        ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Image.asset(
+                          'assets/images/Twitter.png',
+                          width: 50,
+                          height: 50,
+                        ),
+                        const SizedBox(
+                          width: 24,
+                        ),
+                        Image.asset(
+                          'assets/images/Instagram.png',
+                          width: 50,
+                          height: 50,
+                        ),
+                      ],
+                    )
+                  ],
                 ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.green,
-                child: const Center(
+              const VerticalDivider(),
+              const Expanded(
+                flex: 1,
+                child: Center(
                   child: Text(
                     'Metade 2',
-                    style: TextStyle(color: Colors.white),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
