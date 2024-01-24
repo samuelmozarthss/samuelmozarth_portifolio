b78a7d0616a5bda88c34b24f8ece5fc7e8d01bf4
SamuelMozarth <contatosamuelm@gmail.com>
2024-01-24 00:48:53 -0300

    adjustment in tagLinks

---
+++ lib/widgets/my_experience_session.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class MyExperienceSession extends StatefulWidget {
+  final String companyUrl;
+  final String experienceDate;
+  final String experienceTitle;
+  final String employerName;
+  final String experienceDescription;
+  final Map<String, String> tagLinks;
+
+  const MyExperienceSession({
+    super.key,
+    required this.companyUrl,
+    required this.experienceDate,
+    required this.experienceTitle,
+    required this.employerName,
+    required this.experienceDescription,
+    required this.tagLinks,
+  });
+
+  @override
+  State<MyExperienceSession> createState() => _MyExperienceSessionState();
+}
+
+class _MyExperienceSessionState extends State<MyExperienceSession> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> tagLinks = [];
+    widget.tagLinks.forEach((nameTag, url) {
+      tagLinks.add(
+        TagLinks(
+          nameTag: nameTag,
+          url: url,
+        ),
+      );
+      tagLinks.add(const SizedBox(height: 10));
+    });
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: () async {
+          if (await canLaunchUrl(Uri.parse(widget.companyUrl))) {
+            await launchUrlString(widget.companyUrl);
+          } else {
+            throw 'Could not launch ${widget.companyUrl}';
+          }
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 100),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: _isHovered
+                ? Color.fromRGBO(0, 0, 0, 0.010)
+                : Colors.transparent,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.experienceDate,
+                  style: GoogleFonts.inter(
+                    textStyle: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 16,
+                      color: Color.fromRGBO(0, 0, 0, 0.6),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.experienceTitle,
+                        style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16,
+                            color: Color.fromRGBO(0, 0, 0, 0.6),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.employerName,
+                        style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16,
+                            color: Color.fromRGBO(0, 0, 0, 0.6),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.experienceDescription,
+                        style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                            color: Color.fromRGBO(0, 0, 0, 0.6),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.black45,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Wrap(
+                          children: tagLinks,
+                          spacing: 10,
+                          runSpacing: 10,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TagLinks extends StatefulWidget {
+  final String nameTag;
+  final String url;
+
+  TagLinks({required this.nameTag, required this.url});
+
+  @override
+  _TagLinksState createState() => _TagLinksState();
+}
+
+class _TagLinksState extends State<TagLinks> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () async {
+        if (await canLaunchUrl(Uri.parse(widget.url))) {
+          await launchUrlString(widget.url);
+        } else {
+          throw 'Could not launch ${widget.url}';
+        }
+      },
+      onHover: (hover) {
+        setState(() {
+          isHovered = hover;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 4,
+          horizontal: 8,
+        ),
+        child: Text(
+          widget.nameTag,
+          style: GoogleFonts.inter(
+            textStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 10,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/widgets/my_tags_button.dart
+++ lib/widgets/my_tags_button.dart
@@ -3,63 +3,6 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
-class MyTagsButton extends StatefulWidget {
-  const MyTagsButton({
-    super.key,
-  });
-
-  @override
-  State<MyTagsButton> createState() => _MyTagsButtonState();
-}
-
-class _MyTagsButtonState extends State<MyTagsButton> {
-  bool isHovered = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: isHovered ? Colors.black26 : Colors.black54,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: HoverableInkWell(
-            text: 'Flutter',
-            url: 'https://flutter.dev/',
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: isHovered ? Colors.black26 : Colors.black54,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: HoverableInkWell(
-            text: 'Kotlin',
-            url: 'https://kotlinlang.org/',
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: isHovered ? Colors.black26 : Colors.black54,
-            borderRadius: BorderRadius.circular(12),
-          ),
-          child: HoverableInkWell(
-            text: 'Swift',
-            url: 'https://developer.apple.com/swift/',
-          ),
-        ),
-      ],
-    );
-  }
-}
-
 class HoverableInkWell extends StatefulWidget {
   final String text;
   final String url;
