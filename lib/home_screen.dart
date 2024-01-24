57120ad1829c31bbbece50c176face931a7a8efd
SamuelMozarth <contatosamuelm@gmail.com>
2024-01-24 00:50:02 -0300

    new instance of tagLinks

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'widgets/my_animated_menu.dart';
+import 'widgets/my_experience_session.dart';
 import 'widgets/my_strings.dart';
-import 'widgets/my_tags_button.dart';
 import 'widgets/social_media.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -49,7 +48,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
               const VerticalDivider(),
-              Expanded(
+              const Expanded(
                 flex: 1,
                 child: Padding(
                   padding: EdgeInsets.all(10.0),
@@ -58,67 +57,24 @@ class HomeScreen extends StatelessWidget {
                       //AboutTextInPtBR(),
                       //AboutTextInEnUS(),
 
-                      SizedBox(
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              '2018 - PRESENT',
-                              style: GoogleFonts.inter(
-                                textStyle: const TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 16,
-                                  color: Color.fromRGBO(0, 0, 0, 0.6),
-                                ),
-                              ),
-                            ),
-                            const SizedBox(
-                              width: 20,
-                            ),
-                            Expanded(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    'Mobile software engineer',
-                                    style: GoogleFonts.inter(
-                                      textStyle: const TextStyle(
-                                        fontWeight: FontWeight.w800,
-                                        fontSize: 16,
-                                        color: Color.fromRGBO(0, 0, 0, 0.6),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    'EBANX, Curitiba, Brazil',
-                                    style: GoogleFonts.inter(
-                                      textStyle: const TextStyle(
-                                        fontWeight: FontWeight.w800,
-                                        fontSize: 16,
-                                        color: Color.fromRGBO(0, 0, 0, 0.6),
-                                      ),
-                                    ),
-                                  ),
-                                  Text(
-                                    'Working in a multidisciplinary team, in the Mobile domain, using native and hybrid tools for Android and iOS platforms, in the EBANX Go and APP JUNO applications.',
-                                    style: GoogleFonts.inter(
-                                      textStyle: const TextStyle(
-                                        fontWeight: FontWeight.w200,
-                                        fontSize: 16,
-                                        color: Color.fromRGBO(0, 0, 0, 0.6),
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 12,
-                                  ),
-                                  MyTagsButton()
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
+                      MyExperienceSession(
+                          companyUrl: 'ebanx.com',
+                          experienceTitle: 'Mobile software engineer',
+                          employerName: 'EBANX, Curitiba, Brazil',
+                          experienceDescription:
+                              'Working in a multidisciplinary team, in the Mobile domain, using native and hybrid tools for Android and iOS platforms, in the EBANX Go and APP JUNO applications.',
+                          experienceDate: '2018 - PRESENT',
+                          tagLinks: {
+                            'Flutter': 'https://flutter.dev/',
+                            'Dart': 'https://dart.dev/',
+                            'Kotlin': 'https://kotlinlang.org/',
+                            'Swift': 'https://swift.org/',
+                            'Java': 'https://www.java.com/',
+                            'Android': 'https://www.android.com/',
+                            'iOS': 'https://www.apple.com/ios/',
+                            'Git': 'https://git-scm.com/',
+                            'GitHub': '',
+                          })
                     ],
                   ),
                 ),
