471317615313c051b2e7aa6b84f0b28a549ccdc8
SamuelMozarth <contatosamuelm@gmail.com>
2024-01-29 19:17:45 -0300

    create icon in experienceTitle(with bug)

--- lib/widgets/my_experience_session.dart
+++ lib/widgets/my_experience_session.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
@@ -40,110 +41,150 @@ class _MyExperienceSessionState extends State<MyExperienceSession> {
       );
       tagLinks.add(const SizedBox(height: 10));
     });
-    return MouseRegion(
-      onEnter: (_) => setState(() => _isHovered = true),
-      onExit: (_) => setState(() => _isHovered = false),
-      child: GestureDetector(
-        onTap: () async {
-          if (await canLaunchUrl(Uri.parse(widget.companyUrl))) {
-            await launchUrlString(widget.companyUrl);
-          } else {
-            throw 'Could not launch ${widget.companyUrl}';
-          }
-        },
-        child: GestureDetector(
-          onTap: () async {
-            if (await canLaunchUrl(Uri.parse(widget.companyUrl))) {
-              await launchUrlString(widget.companyUrl);
-            } else {
-              throw 'Could not launch ${widget.companyUrl}';
-            }
-          },
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 100),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: _isHovered
-                  ? Color.fromRGBO(0, 0, 0, 0.020)
-                  : Colors.transparent,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      widget.experienceDate,
-                      style: GoogleFonts.inter(
-                        textStyle: const TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 16,
-                          color: Color.fromRGBO(0, 0, 0, 0.6),
-                        ),
+    return GestureDetector(
+      onTap: () async {
+        if (await canLaunchUrl(Uri.parse(widget.companyUrl))) {
+          await launchUrlString(widget.companyUrl);
+        } else {
+          throw 'Could not launch ${widget.companyUrl}';
+        }
+      },
+      child: MouseRegion(
+        onEnter: (_) => setState(() => _isHovered = true),
+        onExit: (_) => setState(() => _isHovered = false),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 100),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            color: _isHovered
+                ? Color.fromRGBO(0, 0, 0, 0.020)
+                : Colors.transparent,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Text(
+                    widget.experienceDate,
+                    style: GoogleFonts.inter(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        color: Color.fromRGBO(0, 0, 0, 0.6),
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    width: 20,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            widget.experienceTitle,
+                            style: GoogleFonts.inter(
+                              textStyle: const TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 16,
+                                color: Color.fromRGBO(0, 0, 0, 0.6),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          ExperienceHeader(
+                            companyUrl: widget.companyUrl,
+                          ),
+                        ],
+                      ),
+                      Text(
+                        widget.employerName,
+                        style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 16,
+                            color: Color.fromRGBO(0, 0, 0, 0.6),
+                          ),
+                        ),
+                      ),
+                      Text(
+                        widget.experienceDescription,
+                        style: GoogleFonts.inter(
+                          textStyle: const TextStyle(
+                            fontWeight: FontWeight.w200,
+                            fontSize: 16,
+                            color: Color.fromRGBO(0, 0, 0, 0.6),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      Wrap(
+                        spacing: 10,
+                        runSpacing: 10,
+                        children: tagLinks,
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      )
+                    ],
                   ),
-                  Expanded(
-                    flex: 5,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          widget.experienceTitle,
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.w800,
-                              fontSize: 16,
-                              color: Color.fromRGBO(0, 0, 0, 0.6),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          widget.employerName,
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.w800,
-                              fontSize: 16,
-                              color: Color.fromRGBO(0, 0, 0, 0.6),
-                            ),
-                          ),
-                        ),
-                        Text(
-                          widget.experienceDescription,
-                          style: GoogleFonts.inter(
-                            textStyle: const TextStyle(
-                              fontWeight: FontWeight.w200,
-                              fontSize: 16,
-                              color: Color.fromRGBO(0, 0, 0, 0.6),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(
-                          height: 12,
-                        ),
-                        Wrap(
-                          children: tagLinks,
-                          spacing: 10,
-                          runSpacing: 10,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                )
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class ExperienceHeader extends StatefulWidget {
+  final String companyUrl;
+
+  const ExperienceHeader({required this.companyUrl});
+  @override
+  State<ExperienceHeader> createState() => _ExperienceHeaderState();
+}
+
+class _ExperienceHeaderState extends State<ExperienceHeader> {
+  double iconSize = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+        onEnter: (PointerEnterEvent event) {
+          setState(() {
+            iconSize = 15;
+          });
+        },
+        onExit: (PointerExitEvent event) {
+          setState(() {
+            iconSize = 10;
+          });
+        },
+        child: GestureDetector(
+            onTap: () async {
+              if (await canLaunchUrl(Uri.parse(widget.companyUrl))) {
+                await launchUrlString(widget.companyUrl);
+              } else {
+                throw 'Could not launch ${widget.companyUrl}';
+              }
+            },
+            child: Icon(
+              Icons.arrow_outward,
+              size: iconSize,
+            )));
   }
 }
 
